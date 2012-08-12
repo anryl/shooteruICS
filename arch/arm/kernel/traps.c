303a36c82eb9d9f5f9aabdbd1a9231ca4b4d26fc
anryl <anryl@kernelcompiler.(none)>
2012-08-12 05:48:33 +0100

    Patched to 3.0.39

--- arch/arm/kernel/traps.c
+++ arch/arm/kernel/traps.c
@@ -478,7 +478,7 @@ do_cache_op(unsigned long start, unsigned long end, int flags)
 			end = vma->vm_end;
 
 		up_read(&mm->mmap_sem);
-		flush_cache_user_range(start, end);
+		flush_cache_user_range(vma, start, end);
 
 #ifdef CONFIG_ARCH_MSM7X27
 		mb();
