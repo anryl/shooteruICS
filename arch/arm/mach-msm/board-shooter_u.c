090b06b4566b3fa5a25715319fbace6e2efda3b0
anryl <anryl@anryl-kernel.(none)>
2012-08-27 13:31:56 +0200

    NEW INTELLIDEMAND GOVERNOR

--- arch/arm/mach-msm/board-shooter_u.c
+++ arch/arm/mach-msm/board-shooter_u.c
@@ -136,9 +136,11 @@
 #endif
 
 #ifdef CONFIG_CPU_FREQ_GOV_ONDEMAND_2_PHASE
-int set_two_phase_freq(int cpufreq);
+ int set_two_phase_freq(int cpufreq);
+ #ifdef CONFIG_CPU_FREQ_GOV_INTELLIDEMAND
+  int id_set_two_phase_freq(int cpufreq);
+ #endif
 #endif
-
 int __init pyd_init_panel(struct resource *res, size_t size);
 
 enum {
@@ -6517,6 +6519,9 @@ static void __init msm8x60_init(struct msm_board_data *board_data)
 
 #ifdef CONFIG_CPU_FREQ_GOV_ONDEMAND_2_PHASE
 	set_two_phase_freq(1134000);
+	#ifdef CONFIG_CPU_FREQ_GOV_INTELLIDEMAND
+	 id_set_two_phase_freq(1134000);
+	#endif
 #endif
 
 	msm8x60_init_tlmm();
